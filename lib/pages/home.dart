d280340c1cff2291eac0de28ca5c2f031847d5ba
r280822a <r280822a@gmail.com>
2023-09-08 17:41:25 +0100

    Altered theme, added selectMode to select multiple notes to delete

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,6 +14,9 @@ class _HomeState extends State<Home> {
   late NotesDatabase notesDB;
   bool loading = true;
 
+  List<bool> selectedCards = [];
+  bool selectMode = false;
+
   @override
   void initState() {
     super.initState();
@@ -30,7 +33,13 @@ class _HomeState extends State<Home> {
     // await notesDB.addNote("Another title", "this is another note");
     // await notesDB.addNote("Title12345678912345678912345", "1234567981234567891234567891234567891234");
     loading = false;
+    selectedCards = List.filled(notesDB.list.length, false, growable: true);
     setState(() {});
+  }
+
+  void selectCard(index){
+    selectedCards[index] = !selectedCards[index];
+    selectMode = selectedCards[index];
   }
 
   @override
@@ -41,7 +50,36 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Home"),
+        leading: selectMode ? IconButton(
+          onPressed: () {
+            selectMode = false;
+            selectedCards = List.filled(selectedCards.length, false, growable: true);
+            setState(() {});
+          },
+          icon: const Icon(Icons.close)
+        ) : null,
+        actions: selectMode ? [
+          IconButton(
+            onPressed: () async {
+              List<Note> notesToDelete = [];
+              for (int i = 0; i < selectedCards.length; i++){
+                if (selectedCards[i]){
+                  notesToDelete.add(notesDB.list[i]);
+                }
+              }
+              for (int i = 0; i < notesToDelete.length; i++){
+                await notesDB.deleteNote(notesToDelete[i]);
+              }
+              selectMode = false;
+              selectedCards = List.filled(selectedCards.length, false, growable: true);
+              setState(() {});
+            }, 
+            icon: const Icon(Icons.delete_outline_outlined)
+          )
+        ] : [],
       ),
+
+      backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
 
       body: MasonryGridView.count(
         padding: const EdgeInsets.all(8),
@@ -54,19 +92,35 @@ class _HomeState extends State<Home> {
 
           return GestureDetector(
             onTap: () async {
-              await Navigator.pushNamed(
-                context,
-                "/note",
-                arguments: {"notesDB":notesDB, "note":notesDB.list[index]},
-              );
+              if (selectMode){
+                selectCard(index);
+              } else{
+                await Navigator.pushNamed(
+                  context,
+                  "/note",
+                  arguments: {"notesDB":notesDB, "note":notesDB.list[index]},
+                );
+              }
+              setState(() {});
+            },
+
+            onLongPress: () {
+              selectCard(index);
               setState(() {});
             },
 
             child: Card(
               elevation: 0,
-              color: Theme.of(context).colorScheme.surfaceVariant,
+              color: Theme.of(context).colorScheme.surface,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+                side: BorderSide(
+                  color: selectedCards[index] ? Theme.of(context).colorScheme.outline : Theme.of(context).colorScheme.surface,
+                  width: selectedCards[index] ? 3 : 0,
+                ),
+              ),
               child: Container(
-                padding: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(12),
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -101,6 +155,7 @@ class _HomeState extends State<Home> {
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           int index = await notesDB.addNote("", "");
+          selectedCards.add(false);
           
           if (mounted) {
             await Navigator.pushNamed(
