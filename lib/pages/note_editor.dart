8a06d0ceb22e513981dd3f6517cb0001efbcfbcb
r280822a <r280822a@gmail.com>
2023-09-18 14:27:31 +0100

    Adds checkbox to where cursor currently is

--- lib/pages/note_editor.dart
+++ lib/pages/note_editor.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:my_notes/notes_db.dart';
 import 'package:intl/intl.dart';
@@ -39,6 +40,8 @@ class _NoteEditorState extends State<NoteEditor> {
   late Note note;
   late NotesDatabase notesDB;
   List<String> descriptionList = [];
+  List<TextEditingController> textControllers = [];
+  // TextEditingController textController = TextEditingController();
 
   TextFormField _textFormField(int index, String initValue, bool hasMultiLines) {
     return TextFormField(
@@ -48,9 +51,10 @@ class _NoteEditorState extends State<NoteEditor> {
         contentPadding: EdgeInsets.zero,
       ),
 
-      key: Key(initValue.toString()),
-      initialValue: initValue,
+      key: Key(initValue.toString() + index.toString()),
+      // initialValue: initValue,
       maxLines: hasMultiLines ? null : 1,
+      controller: textControllers[index],
 
       onChanged: (value) {
         int cbIndex = descriptionList[index].indexOf("▢ ");
@@ -96,6 +100,9 @@ class _NoteEditorState extends State<NoteEditor> {
   }
 
   Widget renderer() {
+    descriptionList.clear();
+    textControllers.clear();
+
     List<Widget> renderedText = [];
     String description = note.description;
 
@@ -107,6 +114,7 @@ class _NoteEditorState extends State<NoteEditor> {
 
     if ((lineSplitText.isEmpty) || (lineSplitText.length == 1)){
       descriptionList.add(description);
+      textControllers.add(TextEditingController(text: description));
       return Expanded(
         child: _textFormField((descriptionList.length - 1), description, true),
       );
@@ -119,11 +127,13 @@ class _NoteEditorState extends State<NoteEditor> {
         if (textBuffer.isNotEmpty){
           String join = textBuffer.join("\n");
           descriptionList.add(join);
+          textControllers.add(TextEditingController(text: join));
           renderedText.add(_textFormField((descriptionList.length - 1), join, true));
           textBuffer = [];
         }
 
         descriptionList.add(line);
+        textControllers.add(TextEditingController(text: line.substring(2)));
         renderedText.add(_checkBox(
           line, 
           (descriptionList.length - 1), 
@@ -145,12 +155,17 @@ class _NoteEditorState extends State<NoteEditor> {
       }
 
       descriptionList.add(value);
+      textControllers.add(TextEditingController(text: value));
       renderedText.add(
         Expanded(
           child: _textFormField((descriptionList.length - 1), value, true)
         ),
       );
     }
+
+    renderedText.add(
+      const SizedBox(height: 60),
+    );
 
     return Expanded(
       child: CustomScrollView(
@@ -176,8 +191,6 @@ class _NoteEditorState extends State<NoteEditor> {
 
     DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(note.time);
     String time = DateFormat('dd MMMM yyyy - hh:mm').format(dateTime);
-
-    descriptionList.clear();
 
     return Scaffold(
       appBar: AppBar(
@@ -213,24 +226,52 @@ class _NoteEditorState extends State<NoteEditor> {
             // Expanded(
             //   child: textFormBuilder(note, notesDB, false),
             // ),
+            // const SizedBox(height: 60),
           ],
         ),
       ),
 
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.white,
+      floatingActionButton: Material(
         child: Row(
           children: [
-            IconButton(
-              onPressed: () {
-                descriptionList.add("▢ ");
-                String newDescription = descriptionList.join("\n");
-                note.description = newDescription;
-                notesDB.updateNote(note);
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child: IconButton(
+                onPressed: () {
+                  // Default offset and index are at end
+                  int offset = 0;
+                  int descIndex = (descriptionList.length - 1);
+                  
+                  // Find currently selected text controller
+                  // Store its offset and index
+                  for (int i = 0; i < textControllers.length; i++){
+                    final int baseOffset = textControllers[i].selection.baseOffset;
+                    if (baseOffset != -1){
+                      offset = baseOffset;
+                      descIndex = i;
+                    }
+                  }
 
-                setState(() {});
-              },
-              icon: const Icon(Icons.add_box_outlined)
+                  // Split text from start till index 
+                  // To find which line to add checkbox (at substringSplit.length)
+                  String substring = descriptionList[descIndex].substring(0, offset);
+                  List<String> substringSplit = substring.split("\n");
+
+                  // Add checkbox
+                  List<String> textSplit = descriptionList[descIndex].split("\n");
+                  textSplit.insert(substringSplit.length, "▢ ");
+                  descriptionList[descIndex] = textSplit.join("\n");
+
+                  // Update note
+                  String newDescription = descriptionList.join("\n");
+                  note.description = newDescription;
+                  notesDB.updateNote(note);
+                  
+                  setState(() {});
+                },
+                icon: const Icon(Icons.add_box_outlined),
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
             ),
           ],
         ),
