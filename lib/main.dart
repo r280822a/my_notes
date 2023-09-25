b2c8225ea4b6cd9fa8aed73baf238b6f4cfc9a2c
r280822a <r280822a@gmail.com>
2023-09-25 13:46:30 +0100

    Removed transparent navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:my_notes/pages/home.dart';
 import 'package:my_notes/pages/note_editor.dart';
 import 'package:my_notes/pages/local_images.dart';
@@ -17,12 +16,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
       builder:(ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-        // Transparent system navbar (WIP)
-        SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-        SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-          systemNavigationBarColor: Colors.transparent,
-        ));
-
         return MaterialApp(
           // Uses dynamic colors, follows system theme
           themeMode: ThemeMode.system,
