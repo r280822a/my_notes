f3d5adb4a8b240813825e8708c84109d6ba9ef96
r280822a <r280822a@gmail.com>
2023-09-25 18:34:38 +0100

    Seperated bottom sheet in docked_action_bar, tweaked its UI

--- lib/widgets/note_editor/docked_action_bar.dart
+++ lib/widgets/note_editor/docked_action_bar.dart
@@ -42,90 +42,11 @@ class DockedActionBar extends StatelessWidget {
                 onPressed: () {
                   showModalBottomSheet(
                     context: context,
-                    builder: (BuildContext context) {
-                      return SizedBox(
-                        height: 120,
-                        child: Column(
-                          children: [
-                            const SizedBox(height: 20),
-                            // Local image button
-                            TextButton(
-                              onPressed: () async {
-                                // Adds local image
-                                Navigator.pop(context);
-                                String imageName = await pickImage();
-              
-                                if (imageName != ""){
-                                  Map<String, int> currentPos = getCurrentTextPos();
-                                  int descIndex = currentPos["descIndex"] as int;
-                                  int offset = currentPos["offset"] as int;
-              
-                                  String link = "[img](assets/$imageName)";
-                                  addNonText(link, descIndex, offset);
-                                }
-                              },
-                              style: TextButton.styleFrom(
-                                foregroundColor: Theme.of(context).colorScheme.onBackground,
-                              ),
-                              child: const Row(
-                                children: [
-                                  Icon(Icons.photo_library_outlined),
-                                  SizedBox(width: 10),
-                                  Text("Add Local Image")
-                                ],
-                              ),
-                            ),
-                            // Network image button
-                            TextButton(
-                              onPressed: () {
-                                // Adds network image
-                                Navigator.pop(context);
-                                Map<String, int> currentPos = getCurrentTextPos();
-                                int descIndex = currentPos["descIndex"] as int;
-                                int offset = currentPos["offset"] as int;
-              
-                                TextEditingController linkController = TextEditingController();
-                                showDialog(
-                                  context: context,
-                                  builder: (context) => AlertDialog(
-                                    title: const Text("Enter image link below"),
-                                    content: TextField(
-                                      decoration: const InputDecoration(
-                                        border: OutlineInputBorder(),
-                                        label: Text("Link"),
-                                      ),
-                                      controller: linkController,
-                                    ),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () {
-                                          if (linkController.text != ""){
-                                            String link = "[img](${linkController.text})";
-                                            addNonText(link, descIndex, offset);
-                                          }
-                                          Navigator.pop(context);
-                                        },
-                                        child: const Text("Ok")
-                                      )
-                                    ],
-                                  ),
-                                );
-                              },
-                              style: TextButton.styleFrom(
-                                foregroundColor: Theme.of(context).colorScheme.onBackground,
-                              ),
-                              child: const Row(
-                                children: [
-                                  Icon(Icons.insert_link_outlined),
-                                  SizedBox(width: 10),
-                                  Text("Add Network Image")
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
+                    builder: (BuildContext context) => AddImageBottomSheet(
+                      pickImage: pickImage,
+                      getCurrentTextPos: getCurrentTextPos,
+                      addNonText: addNonText
+                    ),
                   );
                 },
                 icon: const Icon(Icons.add_photo_alternate_outlined),
@@ -135,6 +56,115 @@ class DockedActionBar extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class AddImageBottomSheet extends StatelessWidget {
+  const AddImageBottomSheet({
+    super.key,
+    required this.pickImage,
+    required this.getCurrentTextPos,
+    required this.addNonText,
+  });
+
+  final Function pickImage;
+  final Function getCurrentTextPos;
+  final Function addNonText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Local image button
+              TextButton(
+                onPressed: () async {
+                  // Adds local image
+                  Navigator.pop(context);
+                  String imageName = await pickImage();
+                    
+                  if (imageName != ""){
+                    Map<String, int> currentPos = getCurrentTextPos();
+                    int descIndex = currentPos["descIndex"] as int;
+                    int offset = currentPos["offset"] as int;
+                    
+                    String link = "[img](assets/$imageName)";
+                    addNonText(link, descIndex, offset);
+                  }
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Theme.of(context).colorScheme.onBackground,
+                ),
+                child: const Row(
+                  children: [
+                    Icon(Icons.photo_library_outlined),
+                    SizedBox(width: 10),
+                    Text(
+                      "Add Local Image",
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+              ),
+              // Network image button
+              TextButton(
+                onPressed: () {
+                  // Adds network image
+                  Navigator.pop(context);
+                  Map<String, int> currentPos = getCurrentTextPos();
+                  int descIndex = currentPos["descIndex"] as int;
+                  int offset = currentPos["offset"] as int;
+                    
+                  TextEditingController linkController = TextEditingController();
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text("Enter image link below"),
+                      content: TextField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          label: Text("Link"),
+                        ),
+                        controller: linkController,
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            if (linkController.text != ""){
+                              String link = "[img](${linkController.text})";
+                              addNonText(link, descIndex, offset);
+                            }
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Ok")
+                        )
+                      ],
+                    ),
+                  );
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Theme.of(context).colorScheme.onBackground,
+                ),
+                child: const Row(
+                  children: [
+                    Icon(Icons.insert_link_outlined),
+                    SizedBox(width: 10),
+                    Text(
+                      "Add Network Image",
+                      style: TextStyle(fontSize: 16)
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
