94e2173473280b5ed346fb49f6b2462f8ecbe332
Rehan Ahmad <r280822a@gmail.com>
2023-11-01 17:25:46 +0000

    Updated version in pubspec.yaml to v4.0.0

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -126,7 +126,7 @@ class _SettingsState extends State<Settings> {
               style: TextStyle(fontSize: 25),
             ),
             subtitle: Text(
-              "v3.4.0",
+              "v4.0.0",
               textAlign: TextAlign.center,
               style: Theme.of(context).textTheme.titleSmall,
             ),
